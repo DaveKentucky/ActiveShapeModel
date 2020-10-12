e4d7f0ad4fa6f30a1b4dad5ed177083e40b6315b
dawimar959 <dawimar959@student.polsl.pl>
2020-10-12 13:52:54 +0200

    Added response to mouse click on displayed image. Added list of landmark points. Comments addition.

--- Code/image.py
+++ Code/image.py
@@ -6,19 +6,26 @@ import sys
 class Image:
 
     def __init__(self, file, size):
+        # file - relative track to image file
+        # size - desired size of image (longer edge)
 
+        # read image from file
         image = cv.imread(file)
         if image is None:
             sys.exit("Failed to load the image")
 
-        self.image = image
-        self.size = size
+        self.image = image  # jpeg image
+        self.size = size    # desired max size of image
+        self.points = []    # array for landmark points
 
         self.height = self.image.shape[0]
         self.width = self.image.shape[1]
+        # scale the image ot desired size
         self.scale_image(self.width, self.height)
 
     def scale_image(self, w, h):
+        # w - original image width in pixels
+        # h - original image height in pixels
 
         if w == h:
             new_w = new_h = self.size
@@ -34,3 +41,11 @@ class Image:
         self.image = cv.resize(self.image, (new_w, new_h))
         self.height = self.image.shape[0]
         self.width = self.image.shape[1]
+
+    def add_landmark_point(self, x, y):
+
+        self.points.append((x, y))
+        cv.circle(self.image, (x, y), 3, (0, 0, 255), -1)
+
+        for point in self.points:
+            print(point)
--- Code/main.py
+++ Code/main.py
@@ -4,11 +4,26 @@ import cv2 as cv
 from image import Image
 
 
-filename = "img2"
+def mouse_click(event, x, y, flags, param):
+    # resolve mouse click on image
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        point = (x, y)
+        # print(point)
+        img.add_landmark_point(x, y)
+        cv.imshow("Image", img.image)
+
+filename = "img6"
 img = Image("Images/Face/" + filename + ".jpg", 400)
 
 print("height: " + str(img.height))
 print("width: " + str(img.width))
 
-cv.imshow("Image", img.image)
-key = cv.waitKey(0)
+cv.namedWindow("Image")
+cv.setMouseCallback("Image", mouse_click, img)
+
+while True:
+    cv.imshow("Image", img.image)
+    key = cv.waitKey(0)
+    if key == 27:
+        break
