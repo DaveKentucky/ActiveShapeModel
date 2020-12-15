6b32a8cf3d20f6b5811940cb4846b1f1a47810b3
dawimar959 <dawimar959@student.polsl.pl>
2020-12-15 11:48:28 +0100

    Added inserting of created point array into database

--- Source/src/main.py
+++ Source/src/main.py
@@ -1,5 +1,5 @@
 import gui
-import database as db
+from database import Database
 
 
 def create_model():
@@ -7,17 +7,22 @@ def create_model():
     model = gui.select_training_data_files()
     if model is not None:     # successfully created shape model
 
-        my_db = db.Database()
-        my_db.insert_model(model)
+        my_db = Database()
+        m_id = my_db.insert_model(model)
+        help_image = None
 
-        for image in model.training_images:   # loop through every training image
-            creator = gui.mark_landmark_points(image)
+        for i, image in enumerate(model.training_images):   # loop through every training image
+            creator = gui.mark_landmark_points(image, help_image)
             if creator is not None:     # received not None creator
                 if len(creator.points) > 0:     # creator's point list is not empty
+                    if i == 0:
+                        help_image = image  # save first image with points as help image
                     if model.shape_info is None:      # there is no shape info set yet
                         info = creator.create_shape_info()  # create new shape info based on created shape
                         model.set_shape_info(info)       # set model's shape info
-                    image.points = creator.points.copy()    # set image points array with marked points
+                        s_id = my_db.insert_shape_info(info, m_id)
+                    image.set_points_from_list(creator.points)    # set image points array with marked points
+                    my_db.insert_image(image, m_id)
 
 
 if __name__ == '__main__':
